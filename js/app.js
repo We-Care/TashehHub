ab963fb28b9710bc5816a8c44f0c31da7783908c
Osama Hanoun <osamahanoun997@gmail.com>
2020-06-07 21:15:12 +0300

    add methods to Account

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 'use strict';
+var currUserID;
+
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -16,10 +18,20 @@ function Account(userName, email, password) {
   this.email = email;
   this.password = password;
   this.userID;
+  this.followedEvents =[];
+  this.userEvents=[];
   Account.all.push(this);
 }
 Account.all = [];
-new Account('osama', 'o@g.com', '00'); // ogj for testing
+Account.prototype.followEvent= function(eventId){
+this.followEvent.push(eventId);// to save the event ID in userEvents followedEvents
+Event.followers.push(currUserID);
+};
+Account.prototype.createEvent= function(eventName, description, imageURL, date, numberOfPeople, requirements){
+    new Event (this.userName,eventName, description, imageURL, date, numberOfPeople, requirements);
+     this.userEvents.push(Event.all.length);   //Event.all.length to save the event ID in userEvents
+};
+new Account('osama', 'o@g.com', '00'); // obj for testing
 document.getElementById('signUpForm').addEventListener('submit', handleSignUp);
 function handleSignUp(event) {
   event.preventDefault();
@@ -38,7 +50,6 @@ function handleSignIn(event) {
 
   let email = event.target.email.value;
   let password = event.target.password.value;
-  let currUserID;
   let i = 0;
   for (i in Account.all) {
     if (Account.all[i].email == email) {
@@ -63,6 +74,8 @@ function handleSignIn(event) {
 }
 
 
+
+
 /////Events Part start//////
 
 
@@ -76,7 +89,7 @@ var eventsArr = [
         creator: usres[getRndInteger(0, usres.length - 1)],
         eventName: 'Aqaba Tasheeh',
 
-        describtion: 'al-ʿAqaba, al-ʿAgaba, pronounced [æl ˈʕæqaba, alˈʕagaba]) is the only coastal city in Jordan and the largest and most populous city on the Gulf of Aqaba.[4] Situated in southernmost Jordan, Aqaba is the administrative centre of the Aqaba Governorate.',
+        description: 'al-ʿAqaba, al-ʿAgaba, pronounced [æl ˈʕæqaba, alˈʕagaba]) is the only coastal city in Jordan and the largest and most populous city on the Gulf of Aqaba.[4] Situated in southernmost Jordan, Aqaba is the administrative centre of the Aqaba Governorate.',
         date: '01/01/2021',
         numberOfPeople: getRndInteger(3, 10),
         status: vertiualEventStatus(),
@@ -88,7 +101,7 @@ var eventsArr = [
         creator: usres[getRndInteger(0, usres.length - 1)],
         eventName: 'Shawerma Tasheeh',
 
-        describtion: 'Shawarm is a dish in Middle Eastern cuisine consisting of meat cut into thin slices, stacked in a cone-like shape, and roasted on a slowly-turning vertical rotisserie or spit. Originally made of lamb or mutton, today\'s shawarma may also be chicken, turkey beef or veal. Thin slices are shaved off the cooked surface as it continuously rotates. Shawarma is one of the world\'s most popular street foods, especially in Egypt and the countries of the Levant, the Arabian Peninsula, and beyond.',
+        description: 'Shawarm is a dish in Middle Eastern cuisine consisting of meat cut into thin slices, stacked in a cone-like shape, and roasted on a slowly-turning vertical rotisserie or spit. Originally made of lamb or mutton, today\'s shawarma may also be chicken, turkey beef or veal. Thin slices are shaved off the cooked surface as it continuously rotates. Shawarma is one of the world\'s most popular street foods, especially in Egypt and the countries of the Levant, the Arabian Peninsula, and beyond.',
 
         date: '12/12/2021',
         numberOfPeople: getRndInteger(3, 10),
@@ -104,19 +117,19 @@ function verticalObjects() {
         var imagePath = eventsArr[i].imagePath;
         var creator = eventsArr[i].creator;
         var eventName = eventsArr[i].eventName;
-        var describtion = eventsArr[i].describtion;
+        var description = eventsArr[i].description;
         var date = eventsArr[i].date;
         var numberOfPeople = eventsArr[i].numberOfPeople;
         var status = eventsArr[i].status;
         var requirements = eventsArr[i].requirements;
-        new Event(imagePath, creator, eventName, describtion, date, numberOfPeople, status, requirements);
+        new Event(imagePath, creator, eventName, description, date, numberOfPeople, status, requirements);
     }
 }
-function Event(imagePath, creator, eventName, describtion, date, numberOfPeople, status, requirements) {
+function Event(imagePath, creator, eventName, description, date, numberOfPeople, status, requirements) {
     this.imagePath = imagePath;
     this.creator = creator;
     this.eventName = eventName;
-    this.describtion = describtion;
+    this.description = description;
     this.date = date;
     this.numberOfPeople = numberOfPeople;
     this.status = status;
@@ -177,7 +190,7 @@ function eventRender() {
         h3E.innerText = Event.all[i].creator;//
         var pE = document.createElement('p');
         figCaptionE.appendChild(pE);
-        pE.innerText = Event.all[i].describtion;//
+        pE.innerText = Event.all[i].description;//
         var ulE = document.createElement('ul');
         figCaptionE.appendChild(ulE);
         for (let index = 0; index < Event.all[i].requirements.length; index++) {
@@ -200,7 +213,7 @@ var eventsArr = [
         creator: usres[getRndInteger(0, usres.length - 1)],
         eventName: 'Aqaba Tasheeh',
 
-        describtion: 'al-ʿAqaba, al-ʿAgaba, pronounced [æl ˈʕæqaba, alˈʕagaba]) is the only coastal city in Jordan and the largest and most populous city on the Gulf of Aqaba.[4] Situated in southernmost Jordan, Aqaba is the administrative centre of the Aqaba Governorate.',
+        description: 'al-ʿAqaba, al-ʿAgaba, pronounced [æl ˈʕæqaba, alˈʕagaba]) is the only coastal city in Jordan and the largest and most populous city on the Gulf of Aqaba.[4] Situated in southernmost Jordan, Aqaba is the administrative centre of the Aqaba Governorate.',
         date: '01/01/2021',
         numberOfPeople: getRndInteger(3, 10),
         status: vertiualEventStatus(),
@@ -212,7 +225,7 @@ var eventsArr = [
         creator: usres[getRndInteger(0, usres.length - 1)],
         eventName: 'Shawerma Tasheeh',
 
-        describtion: 'Shawarm is a dish in Middle Eastern cuisine consisting of meat cut into thin slices, stacked in a cone-like shape, and roasted on a slowly-turning vertical rotisserie or spit. Originally made of lamb or mutton, today\'s shawarma may also be chicken, turkey beef or veal. Thin slices are shaved off the cooked surface as it continuously rotates. Shawarma is one of the world\'s most popular street foods, especially in Egypt and the countries of the Levant, the Arabian Peninsula, and beyond.',
+        description: 'Shawarm is a dish in Middle Eastern cuisine consisting of meat cut into thin slices, stacked in a cone-like shape, and roasted on a slowly-turning vertical rotisserie or spit. Originally made of lamb or mutton, today\'s shawarma may also be chicken, turkey beef or veal. Thin slices are shaved off the cooked surface as it continuously rotates. Shawarma is one of the world\'s most popular street foods, especially in Egypt and the countries of the Levant, the Arabian Peninsula, and beyond.',
 
         date: '12/12/2021',
         numberOfPeople: getRndInteger(3, 10),
@@ -228,22 +241,22 @@ function verticalObjects() {
         var imagePath = eventsArr[i].imagePath;
         var creator = eventsArr[i].creator;
         var eventName = eventsArr[i].eventName;
-        var describtion = eventsArr[i].describtion;
+        var description = eventsArr[i].description;
         var date = eventsArr[i].date;
         var numberOfPeople = eventsArr[i].numberOfPeople;
         var status = eventsArr[i].status;
         var requirements = eventsArr[i].requirements;
-        new Event(imagePath, creator, eventName, describtion, date, numberOfPeople, status, requirements);
+        new Event(imagePath, creator, eventName, description, date, numberOfPeople, status, requirements);
     }
 }
-function Event(imagePath, creator, eventName, describtion, date, numberOfPeople, status, requirements) {
-    this.imagePath = imagePath;
-    this.creator = creator;
+function Event(owner,eventName, description, imageURL, date, numberOfPeople, requirements) {
+    this.owner = owner;
     this.eventName = eventName;
-    this.describtion = describtion;
+    this.description = description;
+    this.imagePath = imageURL;
     this.date = date;
     this.numberOfPeople = numberOfPeople;
-    this.status = status;
+    this.status = true;
     this.requirements = requirements; //array
     Event.all.push(this);
 }
@@ -301,7 +314,7 @@ function eventRender() {
         h3E.innerText = Event.all[i].creator;//
         var pE = document.createElement('p');
         figCaptionE.appendChild(pE);
-        pE.innerText = Event.all[i].describtion;//
+        pE.innerText = Event.all[i].description;//
         var ulE = document.createElement('ul');
         figCaptionE.appendChild(ulE);
         for (let index = 0; index < Event.all[i].requirements.length; index++) {
